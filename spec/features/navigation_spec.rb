c5bc1964651c9bdbf725692685db9991cc05cbe5
Michael Cooper <macoop2363@gmail.com>
2019-12-09 12:38:24 -0700

    Configure testing environment for the new gems

---
+++ spec/features/navigation_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'When I am on any page of the site' do
+  describe 'As a Visitor' do
+    it 'I see a nav bar with links to all pages' do
+      visit '/'
+
+      within 'nav' do
+        click_link 'Register'
+      end
+
+      expect(current_path).to eq('/register')
+
+      within 'nav' do
+        click_link 'Log In'
+      end
+
+      expect(current_path).to eq('/login')
+
+      within 'nav' do
+        click_link 'ComidaCasera'
+      end
+
+      expect(current_path).to eq('/')
+    end
+  end
+
+  describe 'As a Registered User' do
+    it 'I see a nav bar with links to all pages' do
+      user_1 = create(:user)
+
+      
+
+    end
+  end
+end
