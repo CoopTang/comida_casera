b29f5990b6a580c6ba289af0a577ba2e2c9ca48f
Michael Cooper <macoop2363@gmail.com>
2019-12-09 18:02:04 -0700

    Add Recipe table to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_09_195211) do
+ActiveRecord::Schema.define(version: 2019_12_09_235344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,11 @@ ActiveRecord::Schema.define(version: 2019_12_09_195211) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["ingredient_id"], name: "index_nutritions_on_ingredient_id"
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
