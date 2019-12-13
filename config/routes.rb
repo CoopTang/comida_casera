1792a7a6d4f99e173466413f34b641fae27abf02
Michael Cooper <macoop2363@gmail.com>
2019-12-13 11:52:04 -0700

    Scope routes instead of namespace and change Ingredient Nutrition to a resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root 'recipes#index'
+  root 'recipes/recipes#index'
 
-  namespace :recipes do
+  scope module: 'recipes' do
     resources :recipes, only: %i[show index]
   end
 
-  namespace :ingredients do
+  scope module: 'ingredients' do
     resources :ingredients, only: %i[show index] do
-      resources :nutrition, only: %i[show]
+      resource :nutrition, only: %i[show]
     end
   end
 end
