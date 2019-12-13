f3b7481598255b913533ae1ecd7806a685b84d58
Michael Cooper <macoop2363@gmail.com>
2019-12-12 22:38:44 -0700

    Add default mailer routes for sending emails in local development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,4 +61,14 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    domain: 'localhost:3000',
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
