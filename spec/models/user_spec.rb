398a8fa9b7c93fbcdc5f75b55713878761327c5b
Michael Cooper <macoop2363@gmail.com>
2019-12-13 15:36:26 -0700

    Add template for User model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    
+  end
+
+  describe 'relationships' do
+  
+  end
+
+  describe 'methods' do
+    it '::class_method' do
+
+    end
+
+    it '#instance_method' do
+
+    end
+
+  end
 end
