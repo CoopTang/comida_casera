5180e624117a3b5439ee9e5fafa8a060eba70b89
Michael Cooper <macoop2363@gmail.com>
2020-01-03 22:25:29 -0700

    Add validations for attributes and relationships to Ingredients with its respective tests

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -3,11 +3,13 @@ require 'rails_helper'
 RSpec.describe Ingredient, type: :model do
   describe 'validations' do
     it { should validate_presence_of :name }
+    it { should validate_presence_of :image }
   end
 
   describe 'relationships' do
-    it { should have_one :nutrition }
-    # TODO: test for dependent destruction
+    it { should have_many :nutrients }
+    it { should have_many :portions }
+    it { should have_many :aliases }
   end
 
   describe 'methods' do
