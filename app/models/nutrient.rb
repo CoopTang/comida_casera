aa9c71f864512cdfc96d5dd64837cd49fe377f31
Michael Cooper <macoop2363@gmail.com>
2020-01-03 22:49:28 -0700

    Add validations and relationships to Nutrient model with its respective tests

--- app/models/nutrient.rb
+++ app/models/nutrient.rb
@@ -1,2 +1,5 @@
 class Nutrient < ApplicationRecord
+  validates_presence_of :name
+  validates_presence_of :unit
+  validates_presence_of :value
 end
--- spec/models/nutrient_spec.rb
+++ spec/models/nutrient_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe Nutrient, type: :model do
   describe 'validations' do
     it { should validate_presence_of :name }
+    it { should validate_presence_of :unit }
+    it { should validate_presence_of :value }
   end
 
   describe 'relationships' do
