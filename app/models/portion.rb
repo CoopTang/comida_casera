11a14f6560ce268721feff24b965763beed7c595
Michael Cooper <macoop2363@gmail.com>
2020-01-03 23:00:33 -0700

    Add validation for numericality of weight in Portion model with respective test

--- app/models/portion.rb
+++ app/models/portion.rb
@@ -2,5 +2,7 @@ class Portion < ApplicationRecord
   validates_presence_of :name,
                         :weight
 
+  validates_numericality_of :weight
+  
   belongs_to :ingredient
 end
--- spec/models/portion_spec.rb
+++ spec/models/portion_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Portion, type: :model do
   describe 'validations' do
     it { should validate_presence_of :name }
     it { should validate_presence_of :weight }
+    it { should validate_numericality_of :weight }
   end
 
   describe 'relationships' do
