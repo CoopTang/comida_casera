2e8558addf80b0122c0f9e5e92d189844afb1125
Michael Cooper <macoop2363@gmail.com>
2020-01-04 16:56:40 -0700

    Add Nutrient instance method to return the value and unit with its respective test

--- app/models/nutrient.rb
+++ app/models/nutrient.rb
@@ -6,4 +6,11 @@ class Nutrient < ApplicationRecord
   validates_numericality_of :value
 
   belongs_to :ingredient
+
+  def amount
+    # value attribute is overriden by a Rails method
+    val = self.value
+    val = val.to_i if val % 1 == 0
+    "#{val} #{unit}"
+  end
 end
--- spec/models/nutrient_spec.rb
+++ spec/models/nutrient_spec.rb
@@ -13,6 +13,14 @@ RSpec.describe Nutrient, type: :model do
   end
 
   describe 'methods' do
-    
+    before :each do
+      @energy = create(:nutrient_energy)
+      @total_fat = create(:nutrient_total_fat, value: 1.6)
+    end
+
+    it '#amount' do
+      expect(@energy.amount).to eq('100 cal')
+      expect(@total_fat.amount).to eq('1.6 g')
+    end
   end
 end
