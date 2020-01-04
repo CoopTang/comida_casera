56cb2a57200d1d69d577458f4f3aa49924c72af6
Michael Cooper <macoop2363@gmail.com>
2020-01-04 00:02:47 -0700

    Refactor seed file for new database schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,56 +6,65 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-egg = Ingredient.create!(name: 'egg')
-egg.create_nutrition!(
-  serving: 50,
-  calorie: 78,
-  saturated_fat: 1.6,
-  polyunsaturated_fat: 0.7,
-  monounsaturated_fat: 2.0,
-  trans_fat: 0.0,
-  cholesterol: 187,
-  sodium: 62,
-  potassium: 63,
-  total_carbohydrate: 0.6,
-  dietary_fiber: 0.0,
-  sugar: 0.6,
-  protein: 6
+egg = Ingredient.create!(
+  name: 'Egg',
+  image: 'https://www.seriouseats.com/recipes/images/2014/04/20140430-peeling-eggs-10-1500x1125.jpg'
 )
+# egg.create_nutrition!(
+#   serving: 50,
+#   calorie: 78,
+#   saturated_fat: 1.6,
+#   polyunsaturated_fat: 0.7,
+#   monounsaturated_fat: 2.0,
+#   trans_fat: 0.0,
+#   cholesterol: 187,
+#   sodium: 62,
+#   potassium: 63,
+#   total_carbohydrate: 0.6,
+#   dietary_fiber: 0.0,
+#   sugar: 0.6,
+#   protein: 6
+# )
 
-bacon = Ingredient.create!(name: 'bacon')
-bacon.create_nutrition!(
-  serving: 8,
-  calorie: 43,
-  saturated_fat: 1.1,
-  polyunsaturated_fat: 0.4,
-  monounsaturated_fat: 1.5,
-  trans_fat: 0.0,
-  cholesterol: 9,
-  sodium: 137,
-  potassium: 45,
-  total_carbohydrate: 0.1,
-  dietary_fiber: 0.0,
-  sugar: 0.0,
-  protein: 3
+bacon = Ingredient.create!(
+  name: 'Bacon',
+  image: 'https://www.simplyrecipes.com/wp-content/uploads/2019/08/baked-bacon-Lead-1.jpg'
 )
+# bacon.create_nutrition!(
+#   serving: 8,
+#   calorie: 43,
+#   saturated_fat: 1.1,
+#   polyunsaturated_fat: 0.4,
+#   monounsaturated_fat: 1.5,
+#   trans_fat: 0.0,
+#   cholesterol: 9,
+#   sodium: 137,
+#   potassium: 45,
+#   total_carbohydrate: 0.1,
+#   dietary_fiber: 0.0,
+#   sugar: 0.0,
+#   protein: 3
+# )
 
-milk_whole = Ingredient.create!(name: 'milk, whole')
-milk_whole.create_nutrition!(
-  serving: 244,
-  calorie: 148,
-  saturated_fat: 4.6,
-  polyunsaturated_fat: 0.5,
-  monounsaturated_fat: 2.0,
-  trans_fat: 0.0,
-  cholesterol: 24,
-  sodium: 105,
-  potassium: 322,
-  total_carbohydrate: 12,
-  dietary_fiber: 0.0,
-  sugar: 12,
-  protein: 8
+milk_whole = Ingredient.create!(
+  name: 'Milk, whole',
+  image: 'https://i0.wp.com/post.healthline.com/wp-content/uploads/2019/11/milk-soy-hemp-almond-non-dairy-1296x728-header-1296x728.jpg?w=1155&h=1528'
 )
+# milk_whole.create_nutrition!(
+#   serving: 244,
+#   calorie: 148,
+#   saturated_fat: 4.6,
+#   polyunsaturated_fat: 0.5,
+#   monounsaturated_fat: 2.0,
+#   trans_fat: 0.0,
+#   cholesterol: 24,
+#   sodium: 105,
+#   potassium: 322,
+#   total_carbohydrate: 12,
+#   dietary_fiber: 0.0,
+#   sugar: 12,
+#   protein: 8
+# )
 
 baked_ziti = Recipe.create!(
   name: 'Baked ziti',
