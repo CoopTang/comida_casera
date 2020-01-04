2e1153ad050917d0358f35821fcf8d70753b0cf2
Michael Cooper <macoop2363@gmail.com>
2020-01-03 22:58:38 -0700

    Add validation for numericality of value in Nutrient model with respective test

--- app/models/nutrient.rb
+++ app/models/nutrient.rb
@@ -2,4 +2,6 @@ class Nutrient < ApplicationRecord
   validates_presence_of :name,
                         :unit,
                         :value
+
+  validates_numericality_of :value
 end
--- spec/models/nutrient_spec.rb
+++ spec/models/nutrient_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Nutrient, type: :model do
     it { should validate_presence_of :name }
     it { should validate_presence_of :unit }
     it { should validate_presence_of :value }
+    it { should validate_numericality_of :value }
   end
 
   describe 'relationships' do
