47fe169a04ddeeb9e6d49a4a3e78818f80b00d11
Michael Cooper <macoop2363@gmail.com>
2020-01-04 18:11:47 -0700

    Add Ingredient dependents destroy when an Ingredient is destroyed

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -10,6 +10,21 @@ RSpec.describe Ingredient, type: :model do
     it { should have_many :nutrients }
     it { should have_many :portions }
     it { should have_many :aliases }
+
+    it 'should destroy all associated nutrients, portions, and aliases' do
+      egg = create(
+        :ingredient,
+        name: 'Egg',
+        image: 'https://www.seriouseats.com/recipes/images/2014/04/20140430-peeling-eggs-10-1500x1125.jpg'
+      )
+      energy    = create(:nutrient_energy, ingredient: egg)
+      large = create(:portion, name: "1 large", weight: 0.5, ingredient: egg)
+      huevo = create(:alias, name: 'Huevo', ingredient: egg)
+
+      expect{ egg.destroy }.to change { Nutrient.count }.by(-1)
+        .and change { Portion.count }.by(-1)
+        .and change { Alias.count }.by(-1)
+    end
   end
 
   describe 'methods' do
