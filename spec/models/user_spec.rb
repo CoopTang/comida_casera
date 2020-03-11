1f692b96e607981207b6751a9f21fc53450db073
Michael Cooper <macoop2363@gmail.com>
2020-03-11 15:14:40 -0600

    Add validations for email and roles of a User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,24 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'validations' do
-    
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :role }
   end
 
+  describe 'roles' do
+    it 'defualt' do
+      user = User.create!(email: 'test@test.com', password: 'password', role: 0)
+
+      expect(user.role).to eq('default')
+    end
+    
+    it 'admin' do
+      user = User.create!(email: 'test@test.com', password: 'password', role: 1)
+      
+      expect(user.role).to eq('admin')
+    end
+  end
+  
   describe 'relationships' do
   
   end
