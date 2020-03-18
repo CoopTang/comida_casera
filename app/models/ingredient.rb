8f5fb3849770480e5d5d10d5e2c5ba157adfa807
Michael Cooper <macoop2363@gmail.com>
2020-03-17 20:31:55 -0600

    Remove relationship validatations and tests from Ingredients

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,8 +1,4 @@
 class Ingredient < ApplicationRecord
   validates_presence_of :name,
                         :image
-
-  has_many :nutrients, dependent: :destroy
-  has_many :portions, dependent: :destroy
-  has_many :aliases, dependent: :destroy
 end
