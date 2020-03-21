51c57f06053ecafb0b4bce26a0c8ff1b9838a914
Yuhao Gao <uko@cuon.co.jp>
2020-03-21 19:30:28 +0900

    add pantries table (closed #40)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_18_021918) do
+ActiveRecord::Schema.define(version: 2020_03_21_102257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 2020_03_18_021918) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image"
+  end
+
+  create_table "pantries", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_pantries_on_user_id"
   end
 
   create_table "recipes", force: :cascade do |t|
@@ -46,4 +53,5 @@ ActiveRecord::Schema.define(version: 2020_03_18_021918) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "pantries", "users"
 end
